1b9e5e04d2c00ec7ad57fafbada38e2ce0ed298e
Gogoonbuntu <tmddud333@naver.com>
2020-02-17 03:14:42 +0000

    a

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var el = x => document.getElementById(x);
 welcome();
 
-
 function LoadImage(input) {
   var reader = new FileReader();
   reader.onload = function (e) {
@@ -10,16 +9,4 @@ function LoadImage(input) {
   reader.readAsDataURL(input.files[0]);
   $(".instruction").text("processing...");
   $(".filebox").addClass("hide");
-}
-
-function welcome() {
-  var a = Math.random();
-  if(a<0.3){
-    $("#welcome").text("Welcome to our service :)");
-  } else if(a<0.6){
-    $("#welcome").text("Have a great day :D");
-  } else {
-    $("#welcome").text("You look nice today 乃");
-  }
-  
 }
